4288ce18b73e93ae24375de17736e56c2aa1e5d8
Guillaume <guillaumegozillon@gmail.com>
2021-04-06 02:40:06 +0200

    WIP

--- src/dropdown.js
+++ src/dropdown.js
@@ -3,15 +3,36 @@ import { List } from './List.js'
 
 export class Dropdown {
   constructor (data) {
-    this.openBox = this.getIngredient(data)
+    this.openBox = this.openIngredientList(data)
+    this.openInput()
+    this.closeTarget()
   }
 
-  getIngredient (data) {
+  openIngredientList (data) {
+    const arrowList = document.getElementById('arrow')
+    arrowList.addEventListener('click', () => {
+      // test ouverture via l'arrow
+
+      const arrowBtn = document.getElementById('arrow')
+      arrowBtn.classList.add('arrowRotate')
+      new List(data)
+    })
+  }
+
+  openInput () {
     const btnIngredient = document.getElementById('btn-search')
     btnIngredient.addEventListener('click', () => {
+      // test ouverture via l'input
+
       const arrowBtn = document.getElementById('arrow')
-      arrowBtn.classList.add('open')
-      new List(data)
+      arrowBtn.classList.add('arrowRotate')
+      btnIngredient.classList.add('biggerInput')
+    })
+  }
+
+  closeTarget () {
+    document.addEventListener('click', e => {
+      console.log(e.target)
     })
   }
 }
