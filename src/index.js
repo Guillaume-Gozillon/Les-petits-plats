44a52909458b5b5c46ffa9a518ad7e5d10558d81
Guillaume <guillaumegozillon@gmail.com>
2021-04-19 03:52:27 +0200

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-new */
-// import { Card } from './Card.js'
 import { DropdownIngredient } from './dropdown/DropdownIngredient.js'
 import { DropdownAppliance } from './dropdown/DropdownAppliance.js'
 import { DropdownUstensile } from './dropdown/DropdownUstensile.js'
@@ -7,43 +6,42 @@ import { DropdownUstensile } from './dropdown/DropdownUstensile.js'
 const mainHTML = document.querySelector('main')
 const searchInput = document.getElementById('searchbar')
 
-let recettes
+let recipes
 let searchTerm = ''
 
-const fetchRecettes = async () => {
-  recettes = await fetch('./data/recipes.json')
+const fetchData = async () => {
+  recipes = await fetch('./data/recipes.json')
     .then(res => res.json())
 }
 
-const showRecette = async () => {
-  await fetchRecettes()
-  const recetteDOM = recettes.recipes
+const showRecipes = async () => {
+  await fetchData()
+  const data = recipes.recipes
 
-  recetteDOM.forEach(i => {
+  data.forEach(i => {
     new DropdownUstensile(i)
-  });
+  })
 
   mainHTML.innerHTML = (
-    recetteDOM
+    data
       .filter(x => {
         return (x.name).toLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
         (x.description).toLowerCase().includes(searchTerm.toLocaleLowerCase())
       })
-      .map(y => (`
+      .map(recipe => (`
         <div class="container">
-            <div class="empty"></div>
-            <div class="content">
-                <div class="title">
-                    <p>${y.name}</p>
-                    <p><i class="far fa-clock"></i> ${y.time}min</p>
-                </div>
-                <div class="recette">
-                <div class="ingredient">
-                    
-                </div>
-                    <p class="main-para">${y.description}</p>
-                </div>
+          <div class="empty"></div>
+          <div class="content">
+            <div class="title">
+              <p>${recipe.name}</p>
+              <p><i class="far fa-clock"></i> ${recipe.time}min</p>
             </div>
+            <div class="recette">
+            <div class="ingredient">
+            </div>
+              <p class="main-para">${recipe.description}</p>
+            </div>
+          </div>
         </div>`
       ))
   ).join('')
@@ -52,13 +50,13 @@ const showRecette = async () => {
 // SEARCH INPUT
 searchInput.addEventListener('input', e => {
   searchTerm = e.target.value
-  showRecette()
+  showRecipes()
 })
 
 document.getElementById('searchIngredient').addEventListener('click', e => {
   console.log(e.target.textContent)
 })
 
-showRecette()
+showRecipes()
 new DropdownIngredient()
 new DropdownAppliance()
