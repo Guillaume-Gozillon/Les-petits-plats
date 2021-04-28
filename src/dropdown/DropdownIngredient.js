b7904cbf20e63f5f6df4e8b95bc20ce7f392f9e5
Guillaume <guillaumegozillon@gmail.com>
2021-04-28 15:34:40 +0200

    change algo

--- src/dropdown/DropdownIngredient.js
+++ src/dropdown/DropdownIngredient.js
@@ -5,39 +5,6 @@ export class DropdownIngredient {
 
   openIngredientList () {
     const arrowList = document.getElementById('arrowIngredient')
-    const listUL = document.getElementById('searchIngredient')
-
-    listUL.insertAdjacentHTML('afterbegin', `
-    <li>${'Lait de coco'}</li>
-    <li>${'Jus de citron'}</li>
-    <li>${'Crème de coco'}</li>
-    <li>${'Sucre'}</li>
-    <li>${'Glaçons'}</li>
-    <li>${'Thon rouge'}</li>
-    <li>${'Concombre'}</li>
-    <li>${'Tomate'}</li>
-    <li>${'Carotte'}</li>
-    <li>${'Citron vert'}</li>
-    <li>${'Poulet'}</li>
-    <li>${'Coulis'}</li>
-    <li>${'Oignon'}</li>
-    <li>${'Poivron rouge'}</li>
-    <li>${'Huile d\'olive'}</li>
-    <li>${'Riz blanc'}</li>
-    <li>${'Thon en miettes'}</li>
-    <li>${'Oeuf dur'}</li>
-    <li>${'Maïs'}</li>
-    <li>${'Vinaigrette'}</li>
-    <li>${'Pâte feuilletée'}</li>
-    <li>${'Crème fraiche'}</li>
-    <li>${'Gruyère râpé'}</li>
-    <li>${'Moutarde'}</li>
-    <li>${'Pomme'}</li>
-    <li>${'Oeuf'}</li>
-    <li>${'Sucre en poudre'}</li>
-    <li>${'Chocolat au lait'}</li>
-    <li>${'Crème liquide'}</li>
-    <li>${'Beurre'}</li>`)
 
     arrowList.addEventListener('click', e => {
       if (e.target.classList == 'arrow') {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { DropdownIngredient } from './dropdown/DropdownIngredient.js'
 import { DropdownAppliance } from './dropdown/DropdownAppliance.js'
-import { DropdownUstensile } from './dropdown/DropdownUstensile.js'
+// import { DropdownUstensile } from './dropdown/DropdownUstensile.js'
 
 const mainHTML = document.querySelector('main')
 const searchInput = document.getElementById('searchbar')
 const applianceInput = document.getElementById('applianceInput')
+const ingreInput = document.getElementById('inputIngredient')
 
 let recipes
 let searchTerm = ''
@@ -17,19 +18,21 @@ const fetchData = async () => {
 const showRecipes = async () => {
   await fetchData()
   const data = recipes.recipes
+  const listAppliance = document.getElementById('searchAppliance')
 
-  data.forEach(i => {
-    new DropdownUstensile(i)
+  // Filtre les ingredients avec le terme recherché
+  const dataFiltered = data.filter(x => {
+    return x.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      x.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      x.ingredients.some(y => {
+        return y.ingredient.toLowerCase().includes(searchTerm.toLowerCase())
+      })
   })
 
-  mainHTML.innerHTML = (
-    data.filter(x => {
-      return x.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        x.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        x.ingredients.some(y => {
-          return y.ingredient.toLowerCase().includes(searchTerm.toLowerCase())
-        })
-    }).map(recipe => (`
+  const dataAppliance = data.filter(x => x.appliance.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  // Initialise et créer le DOM
+  mainHTML.innerHTML = dataFiltered.map(recipe => (`
         <div class="container">
           <div class="empty"></div>
           <div class="content">
@@ -39,8 +42,8 @@ const showRecipes = async () => {
             </div>
             <div class="recette">
             <div class="ingredient">
-              ${recipe.ingredients
-                .map(x => `<p><span class="bolder">${x.ingredient}:</span> ${x.quantity}${x.unit}</p>`).join('')}
+              ${recipe.ingredients.map(x =>
+                `<p><span class="bolder">${x.ingredient}:</span> ${x.quantity}${x.unit}</p>`).join('')}
             </div>
               <div class="ingredient">
             </div>
@@ -48,61 +51,38 @@ const showRecipes = async () => {
             </div>
           </div>
         </div>`
-    ))
+  )).join('')
+
+  listAppliance.innerHTML = (
+    dataAppliance.map(el => `<li>${el.appliance}</li>`)
   ).join('')
 }
 
+// Initialisation de l'algo
 searchInput.addEventListener('input', e => {
   searchTerm = e.target.value
   showRecipes()
 })
 
-// -----------APPLIANCE -------------------------------
-let searchAppliance = ''
-const listUL = document.getElementById('searchAppliance')
-
-const applianceList = async () => {
-  await fetchData()
-  const data = recipes.recipes
-
-  listUL.innerHTML = (
-    data.filter(x => {
-      return x.name.toLowerCase().includes(searchAppliance.toLowerCase()) ||
-        x.description.toLowerCase().includes(searchAppliance.toLowerCase()) ||
-        x.ingredients.some(y => {
-          return y.ingredient.toLowerCase().includes(searchAppliance.toLowerCase())
-        })
-    })
-      .map(el => `<li>${el.appliance}</li>`)
-  ).join('')
-}
-
-// SEARCH INPUT
-applianceInput.addEventListener('input', e => {
-  searchAppliance = e.target.value
-  applianceList()
-
-  if (e.key === 'ArrowRight') {
-    console.log('IT WORKS')
-  }
+//IICCICICICIICI
+const searchAppliance = document.getElementById('searchAppliance')
+searchAppliance.addEventListener('click', e => {
+  console.log(e.target.textContent);
 })
+
+applianceInput.addEventListener('input', e => {
+  searchTerm = e.target.value
+  showRecipes()
+})
+
 
 // EVENEMENT AU CLICK DU FILTRE
 document.getElementById('searchAppliance').addEventListener('click', e => {
   document.getElementById('tags')
     .insertAdjacentHTML('afterbegin', `<p class="tagAdd">${e.target.textContent}</p>`)
-  const leTest = document.querySelectorAll('.tagAdd')
-
-  const newArr = []
-  for (let i = 0; i < leTest.length; i++) {
-    const element = leTest[i].textContent
-    newArr.push(element)
-  }
-  console.log('element', newArr)
 })
 
 // -----------FIN ---------------
 showRecipes()
-applianceList()
 new DropdownIngredient()
 new DropdownAppliance()
